5cb61d3c7bbe39655a97d7e9ceedb50ec1d35c7b
nikiforovst <stanislav.s.nikiforov@gmail.com>
2018-03-02 20:12:12 +0200

    v1

---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPSERVERTest
+{
+    public partial class Form1 : Form
+    {
+        const int port = 8888;
+        static TcpListener listener;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+                listener.Start();
+                Console.WriteLine("Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    ClientObj clientObj = new ClientObj(client,this);
+
+                    // создаем новый поток для обслуживания нового клиента
+                    Thread clientThread = new Thread(new ThreadStart(clientObj.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (listener != null)
+                    listener.Stop();
+            }
+        }
+
+        public void setTextBoxText(string input)
+        {
+            this.textBox1.Invoke((MethodInvoker)delegate {
+                this.textBox1.AppendText(input + Environment.NewLine);
+            });
+        }
+    }
+}
